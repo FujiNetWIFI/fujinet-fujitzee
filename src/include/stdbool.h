ca80d8af0d101716cd34bc50570cc219ff53c310
Rich Stephens <richswww@gmail.com>
2026-05-11 09:40:01 -0400

    coco: Implement joystick support (#4)

--- src/include/stdbool.h
+++ src/include/stdbool.h
@@ -1,9 +1,16 @@
 #ifndef _STDBOOL_H
 #define _STDBOOL_H
 
+#ifndef true
 #define true 1
-#define false 0
+#endif
 
-typedef unsigned char bool;
+#ifndef false
+#define false 0
+#endif
+
+#ifndef bool
+typedef char bool;
+#endif
 
 #endif
